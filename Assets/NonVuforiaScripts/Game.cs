7e7b6db2c577fbff306ccda91b814fd2772815f7
Rapange <h_ero@hotmail.es>
2017-04-29 16:38:10 -0500

    Juego

---
+++ Assets/NonVuforiaScripts/Game.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Game : MonoBehaviour {
+	private System.Object threadLocker = new System.Object ();
+
+	// Use this for initialization
+	private const int NUM_KEYS = 7;
+	private GameObject[] keys;
+	private List<int> sequence;
+	private string[] key_names;
+	private bool flag, start, ini;
+	private int rnd_num, num_seq;
+	private MaterialPropertyBlock props;
+	void Start () {
+		flag = false;
+		start = false;
+		ini = true;
+		num_seq = -1;
+		key_names = new string[7] {"Do", "Re", "Mi", "Fa", "Sol", "La", "Si"};
+		sequence = new List<int>{};
+		keys = new GameObject[NUM_KEYS];
+		for (int i = 0; i < NUM_KEYS; i++) {
+			keys [i] = GameObject.Find ("ImageTarget/VirtualButton"+key_names[i]+"/"+key_names[i]);
+		}
+		sequence.Add (Random.Range (0, NUM_KEYS));
+		//StartCoroutine (Play ());
+	}
+
+	public void decision(bool passed){
+		if (passed)
+			num_seq++;
+		else {
+			if (ini) {
+				num_seq = 0;
+			} else
+				num_seq = -1;
+		}
+	}
+
+	public bool checkCorrect(int index){
+		lock(threadLocker){
+			if (num_seq > -1 && num_seq < sequence.Count && sequence [num_seq] == index) {
+				//num_seq++;
+				return true;
+			} else {
+				
+				//num_seq = -1;
+				return false;
+			}
+		}
+	}
+
+	IEnumerator finalResult(bool result){
+		if (result) {
+			for (int j = 0; j < 3; j++) {
+				for (int i = 0; i < NUM_KEYS; i++) {
+					ColorGreen (i, true);
+					soundKey (i);
+				}
+				yield return new WaitForSeconds (1.0f);
+				for (int i = 0; i < NUM_KEYS; i++) {
+					ColorGreen (i, false);
+				}
+				yield return new WaitForSeconds (1.0f);
+			}
+		} else {
+			for (int j = 0; j < 3; j++) {
+				for (int i = 0; i < NUM_KEYS; i++) {
+					ColorRed (i, true);
+					soundKey (i);
+				}
+				yield return new WaitForSeconds (1.0f);
+				for (int i = 0; i < NUM_KEYS; i++) {
+					ColorRed (i, false);
+				}
+				yield return new WaitForSeconds (1.0f);
+			}
+		}
+	}
+
+	public void soundKey(int index){
+		keys [index].GetComponent<AudioSource> ().Play ();
+	}
+
+	public void ColorRed(int index, bool activate){
+		props = new MaterialPropertyBlock ();
+		if (activate)
+			props.SetColor ("_Color", Color.red);
+		else
+			props.SetColor ("_Color", Color.white);
+		keys[index].GetComponent<Renderer> ().SetPropertyBlock (props);
+	}
+
+	public void ColorGreen(int index, bool activate){
+		props = new MaterialPropertyBlock ();
+		if (activate)
+			props.SetColor ("_Color", Color.green);
+		else
+			props.SetColor ("_Color", Color.white);
+		keys[index].GetComponent<Renderer> ().SetPropertyBlock (props);
+	}
+
+	public void ColorYellow(int index, bool activate){
+		props = new MaterialPropertyBlock ();
+		if (activate)
+			props.SetColor ("_Color", Color.yellow);
+		else
+			props.SetColor ("_Color", Color.white);
+		keys[index].GetComponent<Renderer> ().SetPropertyBlock (props);
+	}
+
+	IEnumerator PlaySequence(){
+		for (int i = 0; i < sequence.Count; i++) {
+			ColorYellow (sequence [i], true);
+			keys [sequence [i]].GetComponent<AudioSource> ().Play ();
+			yield return new WaitForSeconds (0.5f);
+			ColorYellow (sequence [i], false);
+			yield return new WaitForSeconds (0.5f);
+		}
+		//yield return new WaitForSeconds (3.0f);
+	}
+
+	IEnumerator Play(){
+		for (int i = 0; i < 10; i++) {
+			yield return new WaitForSeconds (2.0f);
+			num_seq = 0;
+			rnd_num = Random.Range (0, NUM_KEYS);
+			sequence.Add (rnd_num);
+			yield return PlaySequence();
+			while (num_seq != sequence.Count) {
+				if (num_seq == -1)
+					goto finale;
+				yield return null;
+			}
+			//PlaySequence ();
+		}
+		finale:
+		if (num_seq == -1) {
+			yield return finalResult (false);
+		} else {
+			yield return finalResult (true);
+		}
+		flag = false;
+		sequence.Clear ();
+		sequence.Add (Random.Range (0, NUM_KEYS));
+		start = false;
+	}
+
+	IEnumerator Repeat(){
+		num_seq = 0;
+		ini = true;
+		yield return new WaitForSeconds (1.0f);
+		while (num_seq < 1) {
+			yield return PlaySequence ();
+		}
+		flag = true;
+		ini = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//keys [0].GetComponent<Renderer> ().material.color = Color.red;
+		//ColorRed (Random.Range(0,1),true);
+		if (!start) {
+			StartCoroutine (Repeat ());
+			start = true;
+		}
+		else if (flag && start) {
+			StartCoroutine (Play ());
+			flag = false;
+		}
+	}
+}
---
+++ Assets/VBMaster.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VBMaster : MonoBehaviour, IVirtualButtonEventHandler {
+
+	private GameObject master;
+	private int index;
+	private bool[] decisions;
+	// Use this for initialization
+	void Start () {
+		index = 0;
+		decisions = new bool[7]{false,false,false,false,false,false,false};
+		VirtualButtonBehaviour[] vbs = GetComponentsInChildren < VirtualButtonBehaviour> ();
+		for (int i = 0; i < vbs.Length; ++i) {
+			vbs [i].RegisterEventHandler (this);
+		}
+		master = GameObject.Find ("ImageTarget/Master");
+	}
+	
+	// Update is called once per frame
+	public void OnButtonPressed (VirtualButtonAbstractBehaviour vb){
+		switch (vb.VirtualButtonName) {
+		case "VirtualButtonDo":
+			//buttonDo.GetComponent<AudioSource> ().Play ();
+			if (master.GetComponent<Game> ().checkCorrect (0)) {
+				decisions [0] = true;
+				master.GetComponent<Game> ().ColorGreen (0, true);
+			}
+			else master.GetComponent<Game> ().ColorRed (0, true);
+			master.GetComponent<Game> ().soundKey (0);
+			break;
+		case "VirtualButtonRe":
+			if (master.GetComponent<Game> ().checkCorrect (1)) {
+				decisions [1] = true;
+				master.GetComponent<Game> ().ColorGreen (1, true);
+			}
+			else master.GetComponent<Game> ().ColorRed (1, true);
+			master.GetComponent<Game> ().soundKey (1);
+			break;
+			break;
+		case "VirtualButtonMi":
+			if (master.GetComponent<Game> ().checkCorrect (2)) {
+				decisions [2] = true;
+				master.GetComponent<Game> ().ColorGreen (2, true);
+			}
+			else master.GetComponent<Game> ().ColorRed (2, true);
+			master.GetComponent<Game> ().soundKey (2);
+			break;
+			break;
+		case "VirtualButtonFa":
+				if (master.GetComponent<Game> ().checkCorrect (3)) {
+				decisions [3] = true;
+					master.GetComponent<Game> ().ColorGreen (3, true);
+				}
+				else master.GetComponent<Game> ().ColorRed (3, true);
+				master.GetComponent<Game> ().soundKey (3);
+				break;
+			break;
+		case "VirtualButtonSol":
+				if (master.GetComponent<Game> ().checkCorrect (4)) {
+				decisions [4] = true;
+					master.GetComponent<Game> ().ColorGreen (4, true);
+				}
+				else master.GetComponent<Game> ().ColorRed (4, true);
+				master.GetComponent<Game> ().soundKey (4);
+				break;
+			break;
+		case "VirtualButtonLa":
+				if (master.GetComponent<Game> ().checkCorrect (5)) {
+				decisions [5] = true;
+					master.GetComponent<Game> ().ColorGreen (5, true);
+				}
+				else master.GetComponent<Game> ().ColorRed (5, true);
+				master.GetComponent<Game> ().soundKey (5);
+				break;
+			break;
+		case "VirtualButtonSi":
+				if (master.GetComponent<Game> ().checkCorrect (6)) {
+				decisions [6] = true;
+					master.GetComponent<Game> ().ColorGreen (6, true);
+				}
+				else master.GetComponent<Game> ().ColorRed (6, true);
+				master.GetComponent<Game> ().soundKey (6);
+				break;
+			break;
+		}
+	}
+
+	public void OnButtonReleased (VirtualButtonAbstractBehaviour vb){
+		switch (vb.VirtualButtonName) {
+			case "VirtualButtonDo":
+				//buttonDo.GetComponent<AudioSource> ().Play ();
+				master.GetComponent<Game> ().ColorRed (0, false);
+				index = 0;
+				//master.GetComponent<Game> ().checkCorrect (0);
+				break;
+			case "VirtualButtonRe":
+				master.GetComponent<Game> ().ColorRed (1, false);
+				index = 1;
+				//master.GetComponent<Game> ().checkCorrect (1);
+				break;
+			case "VirtualButtonMi":
+				master.GetComponent<Game> ().ColorRed (2, false);
+				index = 2;
+				//master.GetComponent<Game> ().checkCorrect (2);
+				break;
+			case "VirtualButtonFa":
+				master.GetComponent<Game> ().ColorRed (3, false);
+				index = 3;
+				//master.GetComponent<Game> ().checkCorrect (3);
+				break;
+			case "VirtualButtonSol":
+				master.GetComponent<Game> ().ColorRed (4, false);
+				index = 4;
+				//master.GetComponent<Game> ().checkCorrect (4);
+				break;
+			case "VirtualButtonLa":
+				master.GetComponent<Game> ().ColorRed (5, false);
+				index = 5;
+				//master.GetComponent<Game> ().checkCorrect (5);
+				break;
+			case "VirtualButtonSi":
+				master.GetComponent<Game> ().ColorRed (6, false);
+				index = 6;
+				//master.GetComponent<Game> ().checkCorrect (6);
+				break;
+			}
+			master.GetComponent<Game> ().checkCorrect (index);
+		master.GetComponent<Game> ().decision (decisions [index]);
+		decisions [index] = false;
+	}
+}
